44588ce7813094ba2463b893fe947e2d5fa074ed
ToxicCicity <ksforunity@gmail.com>
2024-07-13 19:15:51 +0300

    new registers are given the user authority

--- src/main/java/com/example/spring_boot_blog_application/controllers/RegisterController.java
+++ src/main/java/com/example/spring_boot_blog_application/controllers/RegisterController.java
@@ -1,6 +1,8 @@
 package com.example.spring_boot_blog_application.controllers;
 
 import com.example.spring_boot_blog_application.models.Account;
+import com.example.spring_boot_blog_application.models.Authority;
+import com.example.spring_boot_blog_application.repositories.AuthorityRepository;
 import com.example.spring_boot_blog_application.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,11 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Controller
 public class RegisterController {
 
     @Autowired
     private AccountService accountService;
+
+    @Autowired
+    private AuthorityRepository authorityRepository;
 
     public RegisterController(AccountService accountService) {
         this.accountService = accountService;
@@ -29,8 +37,11 @@ public class RegisterController {
     @PostMapping("/register")
     public String registerNewUser(@ModelAttribute Account account) {
         // Save the account to the database
+        Set<Authority> authorities = new HashSet<>();
+        authorityRepository.findById("ROLE_USER").ifPresent(authorities::add);
+        account.setAuthorities(authorities);
         accountService.save(account);
-        return "redirect:/";
+        return "login";
     }
 
 }
