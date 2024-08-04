bb2e8fbd836dfb5d39f1a55e636819ff60ea17c1
ToxicCicity <ksforunity@gmail.com>
2024-08-04 16:04:19 +0300

    unused import removed

--- src/main/java/com/example/spring_boot_blog_application/config/ModelMapperConfig.java
+++ src/main/java/com/example/spring_boot_blog_application/config/ModelMapperConfig.java
@@ -1,6 +1,5 @@
 package com.example.spring_boot_blog_application.config;
 
-import ch.qos.logback.core.model.Model;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
